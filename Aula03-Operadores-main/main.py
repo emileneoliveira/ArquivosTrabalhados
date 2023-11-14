666396d60cddf74d5f7e6e87405092c3a37e8ad2
emileneoliveira <150096555+emileneoliveira@users.noreply.github.com>
2023-11-13 23:31:05 -0300

    Add files via upload

---
+++ Aula03-Operadores-main/main.py
@@ -0,0 +1,121 @@
+# Operadores
+
+# Soma
+x = 2
+x = 2 + 2 # 4
+print(x)
+
+y = 2
+y += 2 # 4
+print(y)
+
+# Subtração
+x = 2
+x = 2 - 2 # 0
+print(x)
+
+y = 2
+y -= 2 # 0
+print(y)
+
+# Multiplicação
+x = 2
+x = 2 * 2 # 4
+print(x)
+
+y = 2
+y *= 2 # 4
+print(y)
+
+# Divisão
+x = 2
+x = 2 / 2 # 1
+print(x)
+
+y = 2
+y /= 2 # 1
+print(y)
+
+# Divisão inteira - resto da divisão
+x = 2
+x = 2 % 2 # 0
+print(x)
+
+y = 2
+y %= 2 # 0
+print(y)
+
+# Potência
+x = 2
+x = 2 ** 2 # 4
+print(x)
+
+y = 2
+y **= 2 # 4
+print(y)
+
+# Quoeficiente inteiro da divisão
+x = 2
+x //= 2 # 1
+print(x)  
+
+# Atribuição
+x = 2
+print(x)
+
+x = 3
+print(pow(x, 2)) # 9
+
+
+# Exercicio 4
+
+
+numero1 = int(input("Digite o primeiro número inteiro: "))
+numero2 = int(input("Digite o segundo número inteiro: "))
+
+soma = numero1 + numero2
+
+print("A soma de", numero1, "+", numero2, "é igual a", soma)
+
+# Atribuição
+x = 2
+x = x + 2
+x += 2
+
+x = x - 2
+x -= 2
+
+x = x * 2
+x *= 2
+
+x = x / 2
+x /= 2
+
+x %= 2
+x **= 2
+x //= 2
+print("x =", x)
+
+# Comparação
+
+print(2 > 1)
+
+print(2 > 3)
+
+print(2 >= 2)
+
+x = 2
+y = 3
+
+print(x < y)
+
+print(x <= y)
+
+print(x == y)
+
+#print(x = y) # erro!
+
+print(x != y)
+
+
+# fim
---
+++ Aula09-Funcoes-main/main.py
@@ -0,0 +1,20 @@
+# Exemplo
+
+def exibirMensagem():
+  frase = "Python é fácil!"
+  print(frase)
+
+#print("Início")
+#exibirMensagem()
+
+
+# Exercicio 22
+
+
+def saudacao(nome):
+  print(f"Olá {nome}, seja bem-vindo!")
+
+nome = input("Digite seu nome: ")
+saudacao(nome)
+
+# Fim
